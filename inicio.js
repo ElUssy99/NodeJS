149a96745ae1e1c0a35ca5b37bb65f6e9cbacb82
carConfigurator <xldaviduson@gmail.com>
2019-02-07 20:24:59 +0100

    commit

--- inicio.js
+++ inicio.js
@@ -1,5 +1,15 @@
 let express = require('express');
+var bodyParser = require('body-parser');
 let app = express();
+
+var urlencodeParser = app.use(bodyParser.urlencoded({ extended: false }))
+
+var users = {
+  "david" : "123",
+  "adri" : "456",
+  "marc" : "789",
+  "jaime" : "101"
+};
 
 app.set('view engine', 'ejs');
 
@@ -11,8 +21,14 @@ app.get('/login', (req, res) => {
   res.render('login');
 });
 
-app.post('/', function (req, res){
-	res.send("rebut");
+app.post('/login', function (req, res){
+  for(var item in users){
+    if(item == req.body.usuario && users[item] == req.body.contra){
+      res.send("El usuario es correcto. Hola " + req.body.usuario + ".");
+      return;
+    }
+    res.send("El usuario es incorrecto.");
+  }
 });
 
 app.listen(4000, () => console.log('Example app listening on port 4000!'));
